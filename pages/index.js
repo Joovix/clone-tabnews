5f632d88640837860dbc9134b763c6c1b1228ee4
João Vitor <91071277+Joovix@users.noreply.github.com>
2026-03-27 12:26:14 +0000

    adiciona scripts `lint:check` e `lint:fix`.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 function Home() {
-    return (
-        <>
-        <h1>Olá á todos, este é um novo teste do git commit --amend</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Olá á todos, este é um novo teste do git commit --amend</h1>
+    </>
+  );
 }
 
-export default Home; 
-
-
-
+export default Home;
